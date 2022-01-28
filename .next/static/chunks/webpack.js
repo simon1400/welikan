880e54035931c915fa243f73af470a134b64b251
Your Name <>
2022-01-28 19:29:11 +0100

    add gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "30277345a301bea37d56"; }
+/******/ 		__webpack_require__.h = function() { return "32fe0a95f4caab3abcba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
