584e9b7163b76ec2372be7d9b0edc9ca3c78e143
Your Name <>
2022-07-07 23:00:53 +0200

    fix link search

--- utility/meilisearch.ts
+++ utility/meilisearch.ts
@@ -1,7 +1,7 @@
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
 
-// const SEARCH_API = "http://search.wellikan.com"
-const SEARCH_API = "http://0.0.0.0:7700"
+const SEARCH_API = "https://search.wellikan.com"
+// const SEARCH_API = "http://0.0.0.0:7700"
 
 const searchClient = instantMeiliSearch(
   SEARCH_API,
